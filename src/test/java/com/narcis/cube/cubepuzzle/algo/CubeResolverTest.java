ae5675f25ceb3d6bfbb5e935e7b4f66c9d29c82a
Catalin Lazar <catalin.lazar@endava.com>
2018-04-05 16:06:30 +0300

    Clean the instance of Position.POSITIONS for each test

--- src/test/java/com/narcis/cube/cubepuzzle/algo/CubeResolverTest.java
+++ src/test/java/com/narcis/cube/cubepuzzle/algo/CubeResolverTest.java
@@ -1,17 +1,23 @@
 package com.narcis.cube.cubepuzzle.algo;
 
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 @RunWith(SpringRunner.class)
 public class CubeResolverTest {
 	private static int[] perfectCube = {1, 2, 2, 2, 2, 1, 1, 1, 2, 2, 1, 1, 2, 1, 2, 1, 1, 2};
 	private static int[] perfectCubeReversed = {1, 2, 1 ,1 ,2, 1, 2, 1 ,1, 2 ,2, 1, 1 ,1, 2, 2, 2, 2};
-	private static CubeResolver cubeResolver = new CubeResolver();
+	private CubeResolver cubeResolver;
 
 	@Test
 	public void testBadCube() {
@@ -28,4 +34,60 @@ public class CubeResolverTest {
 		assertEquals("Fixing the cube...", cubeResolver.findSolution(perfectCubeReversed));
 	}
 
+	@Test
+	public void solvesPerfectCube() throws Exception {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		PrintStream output = new PrintStream(out);
+		System.setOut(output);
+
+		cubeResolver.findSolution(perfectCube);
+
+		assertThat(out.toString("UTF-8")).isEqualToIgnoringNewLines(
+				"Solution: [Move [steps=1, direction=OX_PLUS]\n" +
+				", Move [steps=2, direction=OX_PLUS]\n" +
+				", Move [steps=2, direction=OY_PLUS]\n" +
+				", Move [steps=2, direction=OX_MINUS]\n" +
+				", Move [steps=2, direction=OZ_PLUS]\n" +
+				", Move [steps=1, direction=OX_PLUS]\n" +
+				", Move [steps=1, direction=OZ_MINUS]\n" +
+				", Move [steps=1, direction=OX_PLUS]\n" +
+				", Move [steps=2, direction=OY_MINUS]\n" +
+				", Move [steps=2, direction=OX_MINUS]\n" +
+				", Move [steps=1, direction=OZ_PLUS]\n" +
+				", Move [steps=1, direction=OY_PLUS]\n" +
+				", Move [steps=2, direction=OZ_MINUS]\n" +
+				", Move [steps=1, direction=OX_PLUS]\n" +
+				", Move [steps=2, direction=OZ_PLUS]\n" +
+				", Move [steps=1, direction=OY_MINUS]\n" +
+				", Move [steps=1, direction=OX_PLUS]\n" +
+				", Move [steps=2, direction=OY_PLUS]\n" +
+				"]\n");
+	}
+
+	@Test
+	public void resolvesMultipleInputs() throws Exception {
+		cubeResolver.findSolution(perfectCube);
+		cubeResolver = new CubeResolver();
+		reinitPositions();
+		cubeResolver.findSolution(perfectCubeReversed);
+		cubeResolver = new CubeResolver();
+		reinitPositions();
+		cubeResolver.findSolution(perfectCube);
+	}
+
+	private void reinitPositions() {
+		for (int i = 0; i<3; i++) {
+			for (int j = 0; j < 3; j++) {
+				for (int k = 0; k < 3; k++) {
+					Position.POSITIONS[i][j][k] = null;
+				}
+			}
+		}
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		cubeResolver = new CubeResolver();
+		reinitPositions();
+	}
 }
